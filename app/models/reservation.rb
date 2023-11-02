b8d3e6c468ddb626b334cb73a828c3668ec6d262
katfogy <katfogy@gmail.com>
2023-11-02 17:26:20 +0100

    Linters Error Fixed

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,10 +1,10 @@
 class Reservation < ApplicationRecord
-    belongs_to :user
-    belongs_to :doctor
+  belongs_to :user
+  belongs_to :doctor
 
-    # fields validations
-    validates :user, presence: true
-    validates :doctor, presence: true
-    validates :city, presence: true
-    validates :date, presence: true
+  # fields validations
+  validates :user, presence: true
+  validates :doctor, presence: true
+  validates :city, presence: true
+  validates :date, presence: true
 end
