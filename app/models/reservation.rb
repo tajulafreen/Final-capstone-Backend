cf18ea8e39e3fb00e5a925787a532044e2b86faf
katfogy <katfogy@gmail.com>
2023-11-02 17:19:02 +0100

    User, doctor and reservation relationship done

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,10 @@
 class Reservation < ApplicationRecord
+    belongs_to :user
+    belongs_to :doctor
+
+    # fields validations
+    validates :user, presence: true
+    validates :doctor, presence: true
+    validates :city, presence: true
+    validates :date, presence: true
 end
