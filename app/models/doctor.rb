f56a7448461e97decab933cc2da5041d48ca3370
katfogy <katfogy@gmail.com>
2023-11-02 17:16:53 +0100

    Doctor validated and relationship done

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,6 @@
 class Doctor < ApplicationRecord
+    has_many :reservation, dependent: :destroy
+
+    validates :name, presence: true
+    validates :fee, numericality: { greater_than_or_equal_to: 0.0 }
 end
