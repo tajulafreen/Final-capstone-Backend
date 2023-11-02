70f18ce9744eac080f756b29c58cbd3a9c9a02d9
kit0-0 <kt1767@gmail.com>
2023-11-03 01:18:32 +0530

    Implement index action to fetch all doctors

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::DoctorsController < ApplicationController
   before_action :set_doctor, only: [:show, :update, :destroy]
-    
+  def index
+    @doctors = Doctor.all
+    render json: @doctors
+  end
+  
 end
