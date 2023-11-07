752e3a83e31bb2f2b83be8f22d293a1956fa74e0
Diana Beki <dianabeki54@gmail.com>
2023-11-07 14:08:16 +0300

    Index action to retrieve user's reservations

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,5 +1,13 @@
 class Api::V1::ReservationsController < ApplicationController
+  before_action :set_user, only: [:create]
+  before_action :set_doctor, only: %i[create update destroy]
+
   def index
+    user = User.find(params[:user_id])
+    @reservations = user.reservations.includes(:doctor).order(created_at: :desc)
+    render json: @reservations, include: :doctor, status: :ok
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Reservations not found' }, status: :not_found
   end
 
   def create
