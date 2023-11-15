bc6c1469222a7518a3af7cf3f12927b2d92f63ae
afreen shaik <affushaik9493@gmail.com>
2023-11-15 16:18:24 +0530

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   has_many :reservations, foreign_key: 'user_id', dependent: :destroy
 
   # validations
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
+  
 end
